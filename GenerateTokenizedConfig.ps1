8b9fe4748d5987433ac3466c3a2dc3d9a83d8ea3
Mike <github@barry.io>
2016-12-21 09:59:29 -0500

    Create a tokenized artifact for easier deployment

---
+++ GenerateTokenizedConfig.ps1
@@ -0,0 +1,26 @@
+function XmlDocTransform($xml, $xdt)
+{
+    if (!$xml -or !(Test-Path -path $xml -PathType Leaf)) {
+        throw "File not found. $xml";
+    }
+    if (!$xdt -or !(Test-Path -path $xdt -PathType Leaf)) {
+        throw "File not found. $xdt";
+    }
+
+    Add-Type -LiteralPath "$PSScriptRoot\Microsoft.Web.XmlTransform.dll"
+
+    $xmldoc = New-Object Microsoft.Web.XmlTransform.XmlTransformableDocument;
+    $xmldoc.PreserveWhitespace = $true
+    $xmldoc.Load($xml);
+
+    $transf = New-Object Microsoft.Web.XmlTransform.XmlTransformation($xdt);
+    if ($transf.Apply($xmldoc) -eq $false)
+    {
+        throw "Transformation failed."
+    }
+    $xmldoc.Save($xml);
+}
+
+Copy-Item "$PSScriptRoot\web-user.config" "$PSScriptRoot\web-user-tokenized.config" -Force
+
+XmlDocTransform "$PSScriptRoot\web-user-tokenized.config" "$PSScriptRoot\web-user-transform.xml"
